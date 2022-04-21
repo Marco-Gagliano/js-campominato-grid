c5531b4c88eb22bab909c0fe7e8d56d3ddbdea1a
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-21 23:13:49 +0200

    aggiunta-function-generare-quadrati-e-numeri-più-colore-al-click

--- js/script.js
+++ js/script.js
@@ -17,10 +17,11 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 
 const container = document.querySelector('.container');
-const btnPlay = document.getElementById('play');
+const buttonPlay = document.getElementById('play');
 let numberList = [];
 
-btnPlay.addEventListener('click', function(){
+// ho creato la funzione in cui scelgo la difficolta 
+buttonPlay.addEventListener('click', function(){
   
   container.innerHTML = ' ';
   numberList = [];
@@ -40,20 +41,55 @@ btnPlay.addEventListener('click', function(){
   }
 });
 
-
-init()
+// la funzione "init" con (num )è riferita al numero di celle da creare in base alla difficoltà
 function init(num){
     
-// con la funzione "for" creo la condizione che genero tutti i quadratini
-for(let i = 0; i < (num); i++){
 
-  // la funzione squareEasy genra il quadratino e me lo restituisce
-  const game = squareGame(container, num)
+// con la funzione "for" creo la condizione che genero tutti i quadratini
+for(let i = 1; i <= num; i++){
+
+  // la funzione "createSquare" genero il quadratino e me lo restituisce
+  const game = createSquare(container, num)
+  
+  // genero dentro lo span in HTML il Numero dentro il quadrato
+  game.innerHTML = `<span>${i}</span>`;
 
   // ascolto l'evento click al quadratino generato
   game.addEventListener('click', function(){
 
-    // this è una parola chiave che mi "dice" quale è l'elemento cickato
+    // "this" è una parola chiave che mi dice quale è l'elemento clickato 
     this.classList.add('clicked');
     })}
+}
+
+// creo la funzione in cui genero i numeri dei quadrati in base alla difficoltà
+function createSquare(target, num){
+
+  const sq = document.createElement('div');
+  sq.className = 'square';
+
+  // ottengo un numero univoco
+  // const number = getUniqueRandomNumber(1, 100);
+  
+  if(num === 49){
+    sq.classList.add('square-easy');
+  }
+  else if(num === 81){
+    sq.classList.add('square-medium');
+  }
+  else if(num === 100){
+    sq.classList.add('square-hard');
+  }
+  else if(num === 225){
+    sq.classList.add('square-ultra-nightmare');
+  }
+
+  // aggiungo la classe even o odd in base al calcolo della funzione  dedicata
+  // sq.classList.add(getOddEven(number));
+
+  // appendo l'elemento al container
+  target.append(sq);
+
+  // restituisco l'elemento creato
+  return sq;
 }
