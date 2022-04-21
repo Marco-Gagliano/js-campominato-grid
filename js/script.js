d60d6723e56d082d348b9c83488a56e5954f3455
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-21 18:03:14 +0200

    creazione-parametri-difficoltà

--- js/script.js
+++ js/script.js
@@ -14,3 +14,46 @@ Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dat
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 */
+
+
+const container = document.querySelector('.container');
+const btnPlay = document.getElementById('play');
+let numberList = [];
+
+btnPlay.addEventListener('click', function(){
+  
+  container.innerHTML = ' ';
+  numberList = [];
+  const gameDifficulty = document.getElementById('difficulty').value;
+
+  if(gameDifficulty === 'easy'){
+    init (49)
+  }
+  else if(gameDifficulty === 'medium'){
+    init (81)
+  }
+  else if(gameDifficulty === 'hard'){
+    init (100)
+  }
+  else if(gameDifficulty === 'ultra-nightmare'){
+    init (225)
+  }
+});
+
+
+init()
+function init(num){
+    
+// con la funzione "for" creo la condizione che genero tutti i quadratini
+for(let i = 0; i < (num); i++){
+
+  // la funzione squareEasy genra il quadratino e me lo restituisce
+  const game = squareGame(container, num)
+
+  // ascolto l'evento click al quadratino generato
+  game.addEventListener('click', function(){
+
+    // this è una parola chiave che mi "dice" quale è l'elemento cickato
+    this.classList.add('clicked');
+    })}
+}
