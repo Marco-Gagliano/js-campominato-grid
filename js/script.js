30756deeb57dde9ffe8772e84a3bde83ab0cf2b3
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-21 14:36:44 +0200

    preparazione-pagina

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+
+L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+Difficoltà 1 => tra 1 e 100
+Difficoltà 2 => tra 1 e 81
+Difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+**Consigli del giorno:** :party_wizard:
+**** Creiamo prima una griglia unica (es con 100 quadratini) per  poi dinamicizzare il dato con classi css dedicate
+**** Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+*/
